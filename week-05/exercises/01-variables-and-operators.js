88da6eb6af9d9b9f7b0ecb3ce1b8d9cd13ddf14f
Victoria Negrotto <vnegrotto19@gmail.com>
2023-04-14 18:23:32 -0300

    BaSP-W05-2023: finished variables and operators

--- week-05/exercises/01-variables-and-operators.js
+++ week-05/exercises/01-variables-and-operators.js
@@ -1,1 +1,18 @@
-//Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.//
+console.log('--EXERCISE 1: VARIABLES AND OPERATORS');
+
+//a .Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.//
+
+console.log('exercise 1, a:');
+var a = 10;
+var b = 15;
+console.log (b + a);
+
+console.log('exercise 1, b:');
+var nombre = 'mi nombre es:';
+var soy = 'Victoria';
+console.log (nombre + soy);
+
+console.log('exercise 1, c:');
+var tengo = 'tengo';
+var edad = 'veintinueve años';
+console.log( tengo.length + edad.length);
